c719a3a878e6828fa0a1399717b8c4db52a08682
Huong Do <huongdt29@gmail.com>
2019-05-19 22:47:26 +0700

    Remove icloud syncing

--- AlgoApp/AlgoApp/Source/AppDelegate.swift
+++ AlgoApp/AlgoApp/Source/AppDelegate.swift
@@ -38,6 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         NotificationHelper.shared.showPendingQuestion()
         return true
     }
+}
 
 private extension AppDelegate {
     func configureRealm() {
