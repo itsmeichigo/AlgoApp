de064aab01ba6e9b5f17a8c9d06884f020f61f6b
Huong Do <huongdt29@gmail.com>
2019-11-23 20:36:21 +0700

    Remove firebase

--- AlgoApp/AlgoApp/Source/AppDelegate.swift
+++ AlgoApp/AlgoApp/Source/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Firebase
 import RealmSwift
 import RxSwift
 import RxCocoa
@@ -33,9 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        FirebaseApp.configure()
-        
+                
         configureRealm()
         syncEngine = SyncEngine(objects: [
             SyncObject<QuestionList>(),
