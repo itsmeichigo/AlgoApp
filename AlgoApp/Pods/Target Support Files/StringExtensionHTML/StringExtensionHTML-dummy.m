07861ad8d4c4f4eff8463d1012ad898d7234fb5d
Huong Do <huongdt29@gmail.com>
2020-10-21 10:01:38 +0700

    Stop tracking pods

--- AlgoApp/Pods/Target Support Files/StringExtensionHTML/StringExtensionHTML-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_StringExtensionHTML : NSObject
-@end
-@implementation PodsDummy_StringExtensionHTML
-@end
--- AlgoApp/Pods/Target Support Files/Tags/Tags-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Tags : NSObject
-@end
-@implementation PodsDummy_Tags
-@end
